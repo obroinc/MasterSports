68b1434b63428c9885d98f4d4014036b54b0ef4e
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2020-02-12 13:56:09 +0000

    ApplicayionUser class added to Models Folder and IdenyityUser parameterreplaced by ApplicationUser

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace MasterSports
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
 
